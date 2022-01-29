091b356041cf9737dfeb23613e3a64f2ff138361
sixstring982 <sixstring982@gmail.com>
2022-01-29 09:56:59 -0700

    Redirect to login page when logged out

--- backend/lib/backend_web/controllers/page_controller.ex
+++ backend/lib/backend_web/controllers/page_controller.ex
@@ -2,6 +2,10 @@ defmodule BackendWeb.PageController do
   use BackendWeb, :controller
 
   def index(conn, _params) do
-    render(conn, "index.html")
+    # If the user is not logged in, redirect to the login page.
+    case Plug.Conn.get_session(conn, :current_user_id) do
+      nil -> redirect(conn, to: Routes.session_path(conn, :new))
+      user -> render(conn, "index.html")
+    end
   end
 end
